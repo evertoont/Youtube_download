0f93a665c578e3b6db709ba45e94340d0db8421b
evertoont <evertontbr@gmail.com>
2020-05-12 18:02:43 -0300

    Adiciona texto e campo para entrada da url

--- youtube_download.py
+++ youtube_download.py
@@ -9,6 +9,10 @@ janela = tkinter.Tk()
 janela.title("Download de video do Youtube")
 janela.geometry('350x110+500+300')
 
+lb = tkinter.Label(janela, text="Cole a URL para realizar o download")
+lb.place(x=70, y=5)
 
+ed = tkinter.Entry(janela, width=50)
+ed.place(x=20, y=35)
 
 janela.mainloop()
