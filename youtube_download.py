25b45aba8fc5b032aa7a05ec7fc8d0b4bb803fb3
evertoont <evertontbr@gmail.com>
2020-05-12 18:01:35 -0300

    Adiciona janela de interação

---
+++ youtube_download.py
@@ -0,0 +1,14 @@
+import pytube
+import tkinter
+from tkinter import messagebox
+
+
+# Criação da janela de interação usando kinter
+
+janela = tkinter.Tk()
+janela.title("Download de video do Youtube")
+janela.geometry('350x110+500+300')
+
+
+
+janela.mainloop()
